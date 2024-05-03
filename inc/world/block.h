745072ca823b0022857e84512d6b487613799d89
jochemarends <contact@jochemarends.com>
2024-05-03 22:34:15 +0200

    adding block traits

--- inc/world/block.h
+++ inc/world/block.h
@@ -1,11 +1,34 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+#include <cstdint>
+
 namespace ja {
-    enum class block {
+    /**
+     * Used for representing different block types
+     */
+    enum class block : std::uint8_t {
         empty, 
         grass,
     };
+
+    namespace block_traits {
+        /**
+         * For obtaining the texture index given a block type.
+         *
+         * @param b The block to obtain the texture index for.
+         * @return The texture index.
+         */
+        std::size_t texture_index(block b);
+
+        /**
+         * Determines whether a block is transparent.
+         *
+         * @param b A block type to check for transparency.
+         * @return A boolean value indicating whether the passed block type is transparent.
+         */
+        bool is_transparent(block b);
+    }
 }
 
 #endif
